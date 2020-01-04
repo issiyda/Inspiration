07f26337f076c974b32e43a31adcdc464f5af0ee
issiy <allmylifewriter@gmail.com>
2020-01-04 20:01:57 +0900

    table用のmigrationファイル作成

---
+++ Inspiration/database/migrations/2020_01_04_102621_create_ideas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdeasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ideas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('overflow');
+            $table->string('content');
+            $table->integer('price');
+            $table->boolean('bought_flag');
+            $table->boolean('delete_flag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ideas');
+    }
+}
--- Inspiration/database/migrations/2020_01_04_102835_create_buy_users_table.php
+++ Inspiration/database/migrations/2020_01_04_102835_create_buy_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MypageController extends Migration
+class CreateBuyUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class MypageController extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('buy_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('idea_id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class MypageController extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('buy_users');
     }
 }
