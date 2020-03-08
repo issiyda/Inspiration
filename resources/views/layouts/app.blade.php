14ce08a3bec5d1bce82e60b76a3e26901e34c790
issiy <allmylifewriter@gmail.com>
2020-03-09 00:22:19 +0900

    passReminderToJapanaese
    1.passReminderMailを日本語に

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,3 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -33,6 +11,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!--style css-->
     <link rel="stylesheet" href="{{asset('css/app.css')}}" media="all">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <!--font-awesome-->
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <!--jquery-->
